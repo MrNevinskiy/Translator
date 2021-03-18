3b15bfa75140d76fb1d368982b3543009a03e895
Aleksandr <mr.nevniskiy@gmail.com>
2021-03-18 15:40:04 +0200

    HW7.

--- utils/src/main/java/com/example/utils/network/OnlineLiveData.kt
+++ utils/src/main/java/com/example/utils/network/OnlineLiveData.kt
@@ -3,7 +3,6 @@ package com.example.utils.network
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
-import android.net.NetworkInfo
 import android.net.NetworkRequest
 import androidx.lifecycle.LiveData
 
